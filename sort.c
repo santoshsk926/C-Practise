27a46e489c27433860ab7ce5d78b5d4c909daa98
Santosh <lsantoshsk926@gmail.com>
2018-01-03 20:39:54 +0530

    final sort

--- sort.c
+++ sort.c
@@ -8,17 +8,17 @@ void main()
     scanf("%d",&n);
     printf("enter the elements:");
     for(i=0;i<n;i++)
-        scanf("%d",&a[n]);
+        scanf("%d",&a[i]);
     for(i=0;i<n;i++)
     {
         for(j=i+1;j<n;j++)
         {
-            if(a[i]<a[i+1])
+            if(a[i]>a[j])
              {
                 temp = a[i];
-                a[i] = a[i+1];
+                a[i] = a[j];
                 a[j] = temp;
-              }
+             }
         }
     }
     printf("elements in the sorted order is:");
